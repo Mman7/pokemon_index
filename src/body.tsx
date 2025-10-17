19b0abd5ccc784b23ee00aa0f0b9cf22f5d1a6b7
Eric Man <youzai0427@gmail.com>
2025-10-18 04:13:04 +0800

    finish mockup

--- src/body.tsx
+++ src/body.tsx
@@ -1,9 +1,18 @@
+import ItemCard from "./components/pokemon_item_card";
+import Sidebar from "./components/Sidebar/sidebar";
 export default function Body() {
+  const elements = [];
+  for (let i = 1; i <= 20; i++) {
+    elements.push(
+      <ItemCard key={i} imgSrc="" pokemonName="" pokemonTypes={[]} />,
+    );
+  }
   return (
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex items-center justify-center gap-4 p-6 md:items-start md:justify-around">
+      <div className="drawer-content grid grid-cols-1 justify-items-center gap-6 p-6 md:grid-cols-2 md:justify-items-normal lg:grid-cols-3 xl:grid-cols-5">
         {/* Page content here */}
+        {elements}
       </div>
       <div className="drawer-side">
         <label
@@ -11,14 +20,9 @@ export default function Body() {
           aria-label="close sidebar"
           className="drawer-overlay"
         ></label>
-        <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+        <ul className="menu bg-base-200 text-base-content min-h-full w-70 p-4 sm:w-80">
           {/* Sidebar content here */}
-          <li>
-            <a>Sidebar Item 1</a>
-          </li>
-          <li>
-            <a>Sidebar Item 2</a>
-          </li>
+          <Sidebar />
         </ul>
       </div>
     </div>
