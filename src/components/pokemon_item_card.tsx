90eb5e6e1f131f0365357fc5ae84ea251c9d350f
Eric Man <youzai0427@gmail.com>
2025-10-23 04:34:51 +0800

    added ui skeleton add query for fetching data

--- src/components/pokemon_item_card.tsx
+++ src/components/pokemon_item_card.tsx
@@ -1,32 +1,64 @@
+import { useQuery } from "@tanstack/react-query";
+import { PokemonClient, type Pokemon } from "pokenode-ts";
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import { Link } from "react-router";
+//TODO stats chart
+//TODO custom type badges style
+//TODO id on top left
+const api = new PokemonClient();
 
-interface ItemCardProps {
-  imgSrc: string;
-  pokemonName: string;
-  pokemonTypes: string[];
-}
+const getPokemonDetail = async (name: string) => {
+  const data: Pokemon = await api.getPokemonByName(name);
+  return data;
+};
 
-export default function ItemCard({
-  imgSrc,
-  pokemonName,
-  pokemonTypes,
-}: ItemCardProps) {
-  return (
-    <div className="card rounded-xl shadow-lg backdrop-brightness-125">
-      <figure className="px-6 pt-6">
-        <div className="rounded-2xl bg-black/5">
-          <LazyLoadImage
-            className="w-64"
-            alt={pokemonName}
-            src={
-              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/132.png"
-            }
-          />
-        </div>
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">Bulbasour</h2>
+export default function PokemonCard({ name }: { name: string }) {
+  const { data, error, isLoading } = useQuery({
+    queryKey: [name],
+    queryFn: () => getPokemonDetail(name),
+  });
+
+  if (isLoading)
+    return (
+      <div className="flex w-52 flex-col gap-4">
+        <div className="skeleton h-32 w-full"></div>
+        <div className="skeleton h-4 w-28"></div>
+        <div className="skeleton h-4 w-full"></div>
+        <div className="skeleton h-4 w-full"></div>
       </div>
-    </div>
+    );
+  if (error)
+    return (
+      <div className="grid h-3/4 justify-items-center">
+        An error occurred: {error.message}
+      </div>
+    );
+
+  return (
+    <Link
+      to={`${name}`}
+      state={{
+        pokemon: data,
+      }}
+    >
+      <div className="card rounded-xl shadow-lg backdrop-brightness-120 duration-1000 hover:cursor-pointer hover:backdrop-brightness-140">
+        <figure className="p-8">
+          <div className="indicator rounded-2xl bg-black/5">
+            <div className="indicator-item indicator-center badge badge-primary flex gap-5">
+              <span className="badge">Badge</span>
+              <span className="badge">Badge</span>
+            </div>
+            <span className="indicator-item indicator-bottom indicator-center badge badge-secondary">
+              {data?.name}
+            </span>
+            <LazyLoadImage
+              className="w-64"
+              alt={data?.name}
+              src={data?.sprites.front_default ?? ""}
+            />
+          </div>
+        </figure>
+      </div>
+    </Link>
   );
 }
