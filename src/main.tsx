b933dad8181801e468d7c0d8350446141cfde419
Eric Man <youzai0427@gmail.com>
2025-10-23 04:29:41 +0800

    add react router , query components

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,25 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { ErrorBoundary } from "react-error-boundary";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { BrowserRouter } from "react-router";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ErrorBoundary fallback={<div>Something went wrong</div>}>
-      <App />
-    </ErrorBoundary>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ErrorBoundary
+          fallback={
+            <div className="bg-red-700">React said: Something went wrong</div>
+          }
+        >
+          <App />
+          {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+        </ErrorBoundary>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 );
