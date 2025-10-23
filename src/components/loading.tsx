61d096e9023d8a35e8dcafa1dde458fd6495d9b9
Eric Man <youzai0427@gmail.com>
2025-10-23 18:49:20 +0800

    added loading component

---
+++ src/components/loading.tsx
@@ -0,0 +1,7 @@
+export default function LoadingView() {
+  return (
+    <div className="grid h-3/4 justify-items-center">
+      <span className="loading loading-ring loading-xl m-auto"></span>
+    </div>
+  );
+}
