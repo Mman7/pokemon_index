6fa58d1b32ca9387c750f1b180ccc7a0aefc284e
Eric Man <youzai0427@gmail.com>
2025-10-24 16:35:14 +0800

    added lightmode styled

--- src/pages/homepage/home_page.tsx
+++ src/pages/homepage/home_page.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import PokeBall from "../../assets/Poké_Ball_icon.svg";
 
 export default function Home() {
@@ -8,13 +7,13 @@ export default function Home() {
         <img src={PokeBall} alt="PokeBall" className="w-24" />
         <h1 className="text-4xl font-medium">Welcome to Pokémon Index</h1>
       </span>
-      <h2 className="text-center text-base text-gray-300 sm:text-start">
+      <h2 className="text-center text-lg text-gray-700 sm:text-start dark:text-gray-400">
         Discover the world of Pokémon like never before! From the original 151
         to the newest generations, our index helps you explore every Pokémon’s
         types, abilities, stats, and evolutions. Whether you’re a trainer,
         collector, or fan, there’s something here for everyone.
       </h2>
-      <ul className="list-disc px-6 text-gray-200">
+      <ul className="list-disc px-6 text-gray-700 dark:text-gray-400">
         <li>Browse Pokémon by name, type, or generation.</li>
         <li>View detailed stats, moves, and evolution paths.</li>
         <li>Track your favorite Pokémon and build your personal Pokédex.</li>
