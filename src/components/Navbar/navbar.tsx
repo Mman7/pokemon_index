bdd6a0df3fd94d6b245bf2f02a1b38c3a0d69703
Eric Man <youzai0427@gmail.com>
2025-10-24 19:28:11 +0800

    added: skeleton search result

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -1,7 +1,31 @@
 import Searchbar from "./search_bar";
 import logo from "../../assets/logo.svg";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 
 export default function Navbar() {
+  const [inputValue, setInputValue] = useState("");
+  let navigate = useNavigate();
+  useEffect(() => {
+    if (inputValue !== "") {
+      navigate("/pokemon");
+    }
+  }, [inputValue]);
+
+  const handleChange = (e: any) => {
+    console.log(e.target.value);
+    console.log(typeof e.target.value);
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (inputValue.trim() === "") {
+      alert("Input field cannot be empty!");
+    } else {
+      alert(`Submitted: ${inputValue}`);
+      // Proceed with form submission or other logic
+    }
+  };
   return (
     <div className="navbar bg-base-100 shadow-sm">
       <div className="navbar-start">
@@ -29,7 +53,17 @@ export default function Navbar() {
         </div>
       </div>
       <div className="navbar-center">
-        <Searchbar />
+        <div className="relative z-10 w-64">
+          <Searchbar value={inputValue} handleChange={handleChange} />
+          <ul
+            id="options"
+            className={`${inputValue == "" && "hidden"} absolute w-full overflow-y-auto rounded border border-gray-300 bg-white shadow-lg`}
+          >
+            <li className="cursor-pointer px-4 py-2 text-black hover:bg-gray-100">
+              Apple
+            </li>
+          </ul>
+        </div>
       </div>
       <div className="navbar-end"></div>
     </div>
