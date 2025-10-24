0e391641c41e262af807b9cb44f400c600c59f20
Eric Man <youzai0427@gmail.com>
2025-10-24 19:27:43 +0800

    added: description

---
+++ src/pages/pokemon_details_page.tsx/description.tsx
@@ -0,0 +1,35 @@
+import { TypeBadges } from "../../components/type_badges";
+import type { Pokemon } from "pokenode-ts";
+
+export default function Description({
+  pokemonDetails,
+}: {
+  pokemonDetails: Pokemon;
+}) {
+  const weight = pokemonDetails.weight.toString();
+  let finalWeight = weight.slice(0, -1);
+
+  return (
+    <div className="shadow-x flex flex-col gap-2.75 rounded-2xl p-10 *:text-center md:*:text-start dark:backdrop-brightness-90">
+      <h1 className="text-2xl font-medium capitalize">
+        #{pokemonDetails.id} {pokemonDetails.name}
+      </h1>
+      <div className="flex flex-col gap-1">
+        <h2 className="text-gray-300">Height : {pokemonDetails.height}0 cm</h2>
+        <h2 className="text-gray-300">Weight : {finalWeight} kgs</h2>
+      </div>
+      <div>
+        <h1 className="mb-2 text-2xl font-medium">TYPE</h1>
+        <div className="flex items-center justify-center gap-2 md:justify-start">
+          {pokemonDetails.types.map((type) => (
+            <TypeBadges
+              fontSize="text-lg"
+              key={type.type.name}
+              type={type.type.name}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
