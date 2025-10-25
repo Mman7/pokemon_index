eb4c80b73855d05adbeef17cdd31cde9f916eb63
Eric Man <youzai0427@gmail.com>
2025-10-25 22:39:47 +0800

    update params

--- src/api/pokemon_api.tsx
+++ src/api/pokemon_api.tsx
@@ -2,21 +2,21 @@ import { PokemonClient, type Pokemon } from "pokenode-ts";
 
 const api = new PokemonClient();
 
-export const retrieveData = async ({
+export const getListPokemons = async ({
   pageParam = 0,
 }: {
-  pageParam?: number;
+  pageParam: number;
 }) => {
   const data = await api.listPokemons(pageParam, 20);
   return data;
 };
 
-export const getPokemonDetailByName = async (name: string) => {
+export const getPokemonDetailByName = async ({ name }: { name: string }) => {
   const data: Pokemon = await api.getPokemonByName(name);
   return data;
 };
 
-export const getPokemonDetailByID = async (name: number) => {
-  const data: Pokemon = await api.getPokemonById(name);
+export const getPokemonDetailByID = async ({ id }: { id: number }) => {
+  const data: Pokemon = await api.getPokemonById(id);
   return data;
 };
