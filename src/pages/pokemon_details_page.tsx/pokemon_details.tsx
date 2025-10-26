0ead969272f775bf3536570754dcea5ca41545fb
Eric Man <youzai0427@gmail.com>
2025-10-26 23:34:04 +0800

    restyle, added skeleton

--- src/pages/pokemon_details_page.tsx/pokemon_details.tsx
+++ src/pages/pokemon_details_page.tsx/pokemon_details.tsx
@@ -1,16 +1,17 @@
 import { ArrowLeftFromLine } from "lucide-react";
-import { Link, useLocation } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 import Description from "./description";
 import { Wrapper } from "../../components/wrapper";
 import RadarChart from "../../components/chart";
 import type { Pokemon } from "pokenode-ts";
 import SpiritesShow from "./sprites_show_list";
-// import EvoChain from "./evo_chain";
+import EvoChain from "./evo_chain";
 import { getPokemonSpeciesByName } from "../../api/pokemon_api";
 import { useQuery } from "@tanstack/react-query";
 
 export default function PokemonDetails() {
   const location = useLocation();
+  const nav = useNavigate();
   let state: any = location.state;
   const data: Pokemon = state.pokemon;
   const {
@@ -25,40 +26,66 @@ export default function PokemonDetails() {
     refetchOnWindowFocus: false,
   });
 
+  // EvoChainId and pokemonId is not the same
+  const getEvochainId = () => {
+    const split =
+      specieData?.evolution_chain.url.toString().split("/")[6] ?? "0";
+    return parseInt(split);
+  };
+
   if (state === null) return <div>not found</div>;
-  console.log(data);
   if (error) return <div>Error</div>;
-  if (isLoading) return <div>Loading...</div>;
+
   return (
     <Wrapper>
-      <div className="flex flex-col">
-        <Link
-          className="w-fit pl-6"
-          to={"/pokemon"}
-          state={{
-            pokemon: data.name,
-          }}
+      <div className="flex flex-col gap-6 p-6">
+        <button
+          className="btn btn-wide text-xl"
+          onClick={() => nav("..", { state: { pokemon: data.name } })}
         >
-          <button className="btn btn-wide text-xl">
-            <ArrowLeftFromLine />
-            Pokemon Page
-          </button>
-        </Link>
-        <div className="flex flex-col gap-6 p-6 md:grid md:grid-cols-2">
-          <figure className="flex flex-col items-center gap-6">
+          <ArrowLeftFromLine />
+          Pokemon Page
+        </button>
+        <div className="flex flex-col gap-6 md:grid md:grid-cols-2">
+          <figure className="flex w-full flex-col items-center gap-6 rounded-2xl p-20 shadow-xl backdrop-brightness-150 dark:backdrop-brightness-120">
             <img
-              className="w-full rounded-2xl shadow-xl backdrop-brightness-95 dark:backdrop-brightness-90"
+              className="w-full rounded-2xl bg-black/10"
               src={data.sprites.front_default ?? ""}
             />
           </figure>
-          <Description pokemonDetails={data} speciesDetails={specieData} />
-          <div className="w-full gap-6 rounded-2xl p-6 shadow-xl backdrop-brightness-95">
-            <RadarChart stats={data.stats} pokemonName={data.name} />
-          </div>
+          {isLoading ? (
+            <DescriptionSkeleton />
+          ) : (
+            <Description pokemonDetails={data} speciesDetails={specieData} />
+          )}
+
+          <RadarChart stats={data.stats} pokemonName={data.name} />
+
           <SpiritesShow data={data} />
         </div>
-        {/* <EvoChain name={data.name} id={data.id} /> */}
+        <EvoChain name={data.name} EvoChainId={getEvochainId()} />
       </div>
     </Wrapper>
+  );
+}
+
+export function DescriptionSkeleton() {
+  return (
+    <div className="shadow-x flex flex-col gap-3.5 rounded-2xl px-10 py-6 *:items-center *:text-center md:*:items-start md:*:text-start dark:backdrop-brightness-130">
+      <div className="skeleton h-8 w-28"></div>
+      <div className="skeleton h-8 w-full"></div>
+      <div className="skeleton h-8 w-28"></div>
+      <div className="skeleton h-8 w-28"></div>
+      <div className="skeleton h-8 w-18"></div>
+      <div className="flex flex-row flex-wrap gap-3">
+        <div className="skeleton h-8 w-28"></div>
+        <div className="skeleton h-8 w-28"></div>
+      </div>
+      <div className="skeleton h-8 w-18"></div>
+      <div className="flex flex-row flex-wrap gap-3">
+        <div className="skeleton h-8 w-14"></div>
+        <div className="skeleton h-8 w-14"></div>
+      </div>
+    </div>
   );
 }
