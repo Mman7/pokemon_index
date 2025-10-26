c938769f083051b2901046770d7423cba1950edb
Eric Man <youzai0427@gmail.com>
2025-10-26 23:33:15 +0800

    updated style

--- src/pages/pokemon_details_page.tsx/description.tsx
+++ src/pages/pokemon_details_page.tsx/description.tsx
@@ -12,20 +12,16 @@ export default function Description({
   let finalWeight = weight.slice(0, -1);
 
   return (
-    <div className="shadow-x flex flex-col gap-3.5 rounded-2xl px-10 py-6 backdrop-brightness-95 *:text-center md:*:text-start dark:backdrop-brightness-130">
+    <div className="shadow-x flex flex-col gap-3.5 rounded-2xl px-10 py-6 *:items-center *:text-center md:*:items-start md:*:text-start dark:backdrop-brightness-130">
       <h1 className="text-2xl font-bold capitalize">
         #{pokemonDetails.id} {pokemonDetails.name}
       </h1>
-      <h2 className="hidden text-gray-300 xl:block">
+      <h2 className="hidden tracking-tight text-gray-500 xl:block dark:text-gray-300">
         {speciesDetails?.flavor_text_entries?.[6]?.flavor_text ?? ""}
       </h2>
-      <div className="flex flex-col gap-1">
-        <h2 className="text-gray-600 dark:text-gray-300">
-          Height : {pokemonDetails.height}0 cm
-        </h2>
-        <h2 className="text-gray-600 dark:text-gray-300">
-          Weight : {finalWeight} kgs
-        </h2>
+      <div className="flex flex-col gap-1 font-medium text-gray-600 *:text-2xl dark:text-gray-300">
+        <h2>Height : {pokemonDetails.height}0 cm</h2>
+        <h2>Weight : {finalWeight} kgs</h2>
       </div>
       <div>
         <h1 className="mb-2 text-2xl font-bold">TYPE</h1>
@@ -40,7 +36,7 @@ export default function Description({
         </div>
         <div className="pt-3">
           <h1 className="mb-2 text-2xl font-bold">Ability</h1>
-          <div className="m-0 flex flex-row items-center justify-center gap-2 md:justify-start">
+          <div className="m-0 flex flex-row flex-wrap items-center justify-center gap-2 md:justify-start">
             {pokemonDetails.abilities.map((type) => (
               <div
                 key={type.ability.name}
