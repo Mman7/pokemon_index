5f51866cfccdb5ecd0e7aac6d0a3910750b1a73f
Eric Man <youzai0427@gmail.com>
2025-10-26 18:36:05 +0800

    added get species

--- src/pages/pokemon_details_page.tsx/pokemon_details.tsx
+++ src/pages/pokemon_details_page.tsx/pokemon_details.tsx
@@ -5,21 +5,35 @@ import { Wrapper } from "../../components/wrapper";
 import RadarChart from "../../components/chart";
 import type { Pokemon } from "pokenode-ts";
 import SpiritesShow from "./sprites_show_list";
-
-//TODO add more content
+// import EvoChain from "./evo_chain";
+import { getPokemonSpeciesByName } from "../../api/pokemon_api";
+import { useQuery } from "@tanstack/react-query";
 
 export default function PokemonDetails() {
   const location = useLocation();
   let state: any = location.state;
+  const data: Pokemon = state.pokemon;
+  const {
+    data: specieData,
+    error,
+    isLoading,
+  } = useQuery({
+    queryKey: [`${data.name}Specie`],
+    queryFn: () => getPokemonSpeciesByName({ name: data.name }),
+    staleTime: 1000 * 60 * 5,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+  });
 
   if (state === null) return <div>not found</div>;
-  const data: Pokemon = state.pokemon;
-
+  console.log(data);
+  if (error) return <div>Error</div>;
+  if (isLoading) return <div>Loading...</div>;
   return (
     <Wrapper>
       <div className="flex flex-col">
         <Link
-          className="pl-6"
+          className="w-fit pl-6"
           to={"/pokemon"}
           state={{
             pokemon: data.name,
@@ -30,19 +44,20 @@ export default function PokemonDetails() {
             Pokemon Page
           </button>
         </Link>
-        <div className="flex flex-col gap-12 p-6 md:grid md:grid-cols-2">
+        <div className="flex flex-col gap-6 p-6 md:grid md:grid-cols-2">
           <figure className="flex flex-col items-center gap-6">
             <img
               className="w-full rounded-2xl shadow-xl backdrop-brightness-95 dark:backdrop-brightness-90"
               src={data.sprites.front_default ?? ""}
             />
           </figure>
-          <Description pokemonDetails={data} />
+          <Description pokemonDetails={data} speciesDetails={specieData} />
           <div className="w-full gap-6 rounded-2xl p-6 shadow-xl backdrop-brightness-95">
             <RadarChart stats={data.stats} pokemonName={data.name} />
           </div>
           <SpiritesShow data={data} />
         </div>
+        {/* <EvoChain name={data.name} id={data.id} /> */}
       </div>
     </Wrapper>
   );
