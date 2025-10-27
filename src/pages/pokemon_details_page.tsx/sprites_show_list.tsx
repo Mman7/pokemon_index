544a3d23ff846c257107dc61cfb883a043f63c48
Eric Man <youzai0427@gmail.com>
2025-10-27 20:42:38 +0800

    fixed: image src not found

--- src/pages/pokemon_details_page.tsx/sprites_show_list.tsx
+++ src/pages/pokemon_details_page.tsx/sprites_show_list.tsx
@@ -1,49 +1,48 @@
 import type { Pokemon } from "pokenode-ts";
 
 export default function SpiritesShow({ data }: { data: Pokemon }) {
-  const female = data.sprites.front_female !== null;
-  const shiny = data.sprites.back_shiny !== null;
-
+  const female: boolean = data.sprites.front_female !== null;
+  const shiny: boolean = data.sprites.back_shiny !== null;
   return (
     <div className="w-full gap-6 rounded-2xl p-6 shadow-xl dark:backdrop-brightness-95">
       <h1 className="mb-3 text-2xl font-bold">Variants</h1>
       <div className="md:grid-row-2 flex flex-wrap items-center justify-center gap-4 md:grid md:grid-cols-2">
         <SpiritesItem
           className="bg-primary/70 dark:bg-primary/30"
-          src={data.sprites.front_default ?? ""}
+          src={data.sprites.front_default ?? null}
           alt={`${data.name}-front`}
         />
         <SpiritesItem
           className="bg-primary/70 dark:bg-primary/30"
-          src={data.sprites.back_default ?? ""}
+          src={data.sprites.back_default ?? null}
           alt={`${data.name}-back`}
         />
 
         {female && (
           <SpiritesItem
             className="bg-secondary/70 dark:bg-secondary/30"
-            src={data.sprites.front_female ?? ""}
+            src={data.sprites.front_female ?? null}
             alt={`${data.name}-front-female`}
           />
         )}
         {female && (
           <SpiritesItem
             className="bg-secondary/70 dark:bg-secondary/30"
-            src={data.sprites.back_female ?? ""}
+            src={data.sprites.back_female ?? null}
             alt={`${data.name}-front-female-shiny`}
           />
         )}
         {shiny && (
           <SpiritesItem
             className="bg-accent/70 dark:bg-accent/30"
-            src={data.sprites.front_shiny ?? ""}
+            src={data.sprites.front_shiny ?? null}
             alt={`${data.name}-front-female-shiny`}
           />
         )}
         {shiny && (
           <SpiritesItem
             className="bg-accent/70 dark:bg-accent/30"
-            src={data.sprites.back_shiny ?? ""}
+            src={data.sprites.back_shiny ?? null}
             alt={`${data.name}-back-female-shiny`}
           />
         )}
@@ -57,15 +56,15 @@ function SpiritesItem({
   alt,
   className,
 }: {
-  src: string;
+  src: string | null;
   alt: string;
   className: string;
 }) {
   return (
     <div
-      className={`flex rounded-2xl p-4 shadow-xl backdrop-brightness-90 ${className}`}
+      className={`${src == "" && "hidden"} flex rounded-2xl p-4 shadow-xl backdrop-brightness-90 ${className}`}
     >
-      <img className="w-3xs" src={src} alt={alt} />
+      <img className="w-3xs" src={src === null ? undefined : src} alt={alt} />
     </div>
   );
 }
