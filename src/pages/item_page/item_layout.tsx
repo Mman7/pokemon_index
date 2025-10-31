608e9163edaa5191f076a40162e3d9f2822e311d
Eric Man <youzai0427@gmail.com>
2025-10-31 21:53:50 +0800

    refactor list into components

--- src/pages/item_page/item_layout.tsx
+++ src/pages/item_page/item_layout.tsx
@@ -1,15 +1,18 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { getItemList } from "../../api/pokemon_api";
 import LoadingView from "../../components/loading";
-import ItemCard from "./item_card";
-import { Fragment, useEffect, useRef } from "react";
+import { Fragment, useEffect, useRef, useState } from "react";
 import { useInView } from "react-intersection-observer";
 import { Outlet, useLocation, useMatch } from "react-router";
+import ItemList from "./item_list";
+import type { NamedAPIResource } from "pokenode-ts";
 
 export default function ItemLayout() {
   const { ref, inView } = useInView();
   const refs = useRef<{ [key: string]: HTMLDivElement | null }>({});
   const location = useLocation();
+  const [searchData, setSearchData] = useState<NamedAPIResource[]>([]);
+
   const {
     data,
     isLoading,
@@ -29,14 +32,20 @@ export default function ItemLayout() {
     },
   });
 
-  useEffect(() => {
-    // back to the item with name
+  // back to the item with name
+  const restoreScrollPosition = () => {
     if (location.state && location?.state.item) {
       refs.current[location?.state.item.name]?.scrollIntoView({
         behavior: "instant",
         block: "center",
       });
     }
+  };
+
+  useEffect(() => {
+    let state = location.state;
+    if (state && state.searchData) setSearchData(state.searchData);
+    restoreScrollPosition();
   }, [location.state]);
 
   useEffect(() => {
@@ -49,25 +58,22 @@ export default function ItemLayout() {
   const isitemDetailPage = useMatch("/item/:name");
   const items = data?.pages.flatMap((p) => p.results) ?? [];
 
+  const searchDataEmpty = searchData.length < 1;
+
   return (
     <Fragment>
-      <div
+      <section
         className={`${isitemDetailPage && "hidden"} grid grid-cols-1 justify-items-center gap-6 p-6 md:grid-cols-2 md:justify-items-normal lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5`}
       >
-        {items.map((item, index) => (
-          <div
-            ref={(el) => {
-              refs.current[item.name] = el;
-            }}
-            key={index}
-          >
-            <ItemCard item={item} />
-          </div>
-        ))}
-      </div>
-      {!isitemDetailPage && (
-        <div ref={ref}>{!isFetchingNextPage && <LoadingView />}</div>
-      )}
+        {/* Search List */}
+        <ItemList items={searchData} isShowing={!searchDataEmpty} refs={refs} />
+        {/* Normal List */}
+        <ItemList items={items} isShowing={searchDataEmpty} refs={refs} />
+        {/*  Reached this div will fetch more data */}
+        {searchDataEmpty && (
+          <div ref={ref}>{!isFetchingNextPage && <LoadingView />}</div>
+        )}
+      </section>
       <Outlet />
     </Fragment>
   );
