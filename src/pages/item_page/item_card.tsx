c6b0062463f64c6bc4a30fa5ecce7b6678736289
Eric Man <youzai0427@gmail.com>
2025-11-01 19:43:44 +0800

    refactor

--- src/pages/item_page/item_card.tsx
+++ src/pages/item_page/item_card.tsx
@@ -2,6 +2,8 @@ import { useQuery } from "@tanstack/react-query";
 import type { NamedAPIResource } from "pokenode-ts";
 import { getItemByName } from "../../api/pokemon_api";
 import { Link, useLocation } from "react-router";
+import ItemImg from "../../components/item_img";
+import ItemCardSkeleton from "../../components/item_card_skeleton";
 
 export default function ItemCard({ item }: { item: NamedAPIResource }) {
   const location = useLocation();
@@ -11,16 +13,8 @@ export default function ItemCard({ item }: { item: NamedAPIResource }) {
     queryFn: () => getItemByName({ name: item.name }),
   });
 
-  if (isLoading) {
-    const defaultStyle = "skeleton bg-gray-300 dark:bg-gray-700";
-    return (
-      <div className="flex w-52 flex-col gap-4">
-        <div className={`${defaultStyle} h-32 w-full`}></div>
-        <div className={`${defaultStyle} h-8 w-full`}></div>
-        <div className={`${defaultStyle} h-8 w-full`}></div>
-      </div>
-    );
-  }
+  if (isLoading) return <ItemCardSkeleton />;
+
   return (
     <Link
       to={`/item/${data?.name}`}
@@ -30,13 +24,7 @@ export default function ItemCard({ item }: { item: NamedAPIResource }) {
       }}
     >
       <div className="card gap-2.75 rounded-xl p-6 shadow-lg backdrop-brightness-120 duration-1000 hover:cursor-pointer hover:backdrop-brightness-140">
-        <figure className="flex items-center justify-center">
-          {data?.sprites.default ? (
-            <img className="w-32" src={data?.sprites.default} alt={item.name} />
-          ) : (
-            <div className="h-32 p-5 text-center text-6xl">?</div>
-          )}
-        </figure>
+        <ItemImg src={data?.sprites.default} alt={data?.name} />
         <div className="flex items-center justify-center gap-1">
           <div
             className={`badge bg-gray-500 p-2 text-sm font-medium text-gray-300 capitalize dark:bg-gray-600 dark:text-gray-400`}
