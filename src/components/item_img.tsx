cf5eaf84704406600c0ba811ed2c5ad70556d488
Eric Man <youzai0427@gmail.com>
2025-11-01 19:41:55 +0800

    added custom class name

--- src/components/item_img.tsx
+++ src/components/item_img.tsx
@@ -1,14 +1,16 @@
 export default function ItemImg({
   src,
   alt,
+  imgClassName,
 }: {
   src: string | undefined;
   alt: string | undefined;
+  imgClassName?: string;
 }) {
   return (
-    <figure className="flex items-center justify-center">
+    <figure className="flex items-center justify-center rounded-xl bg-gray-200/50 dark:bg-black/20">
       {src ? (
-        <img className="w-32" src={src} alt={alt} />
+        <img className={`w-32 ${imgClassName}`} src={src} alt={alt} />
       ) : (
         <div className="h-32 p-5 text-center text-6xl">?</div>
       )}
