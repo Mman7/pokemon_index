347baa3b5a04fc189925098626aa3e5a819d11db
EricMan <youzai0427@gmail.com>
2025-11-04 16:50:32 +0800

    added align

--- src/components/item_card_skeleton.tsx
+++ src/components/item_card_skeleton.tsx
@@ -1,5 +1,5 @@
 export default function ItemCardSkeleton() {
-  const defaultStyle = "skeleton bg-gray-300 dark:bg-gray-700";
+  const defaultStyle = "skeleton ml-auto bg-gray-300 dark:bg-gray-700";
   return (
     <div className="flex w-52 flex-col gap-4">
       <div className={`${defaultStyle} h-32 w-full`}></div>
