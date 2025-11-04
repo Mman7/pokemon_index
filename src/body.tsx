996f944782c85dd6fda30f332fd603372270eb49
EricMan <youzai0427@gmail.com>
2025-11-04 16:50:07 +0800

    refactor to createBrowserRouter

--- src/body.tsx
+++ src/body.tsx
@@ -1,30 +1,12 @@
-import { Navigate, Route, Routes } from "react-router";
+import { Outlet } from "react-router";
 import Sidebar from "./components/Sidebar/sidebar";
-import PokemonList from "./pages/pokemon_page.tsx/pokemon_layout";
-import PokemonDetails from "./pages/pokemon_page.tsx/pokemon_details";
-import Home from "./pages/homepage/home_page";
-import ItemLayout from "./pages/item_page/item_layout";
-import ItemDetails from "./pages/item_page/item_details";
-import BerryLayout from "./pages/berry_page/berry_layout";
-import BerryDetails from "./pages/berry_page/berry_details";
+
 export default function Body() {
   return (
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Navigate to="/" />} />
-          <Route path="/pokemon" element={<PokemonList />}>
-            <Route path=":name" element={<PokemonDetails />} />
-          </Route>
-          <Route path="/berry" element={<BerryLayout />}>
-            <Route path=":name" element={<BerryDetails />} />
-          </Route>
-          <Route path="/item" element={<ItemLayout />}>
-            <Route path=":name" element={<ItemDetails />} />
-          </Route>
-        </Routes>
+      <div className="drawer-content overflow-auto">
+        <Outlet />
       </div>
       <div className="drawer-side">
         <label
--- src/main.tsx
+++ src/main.tsx
@@ -4,24 +4,87 @@ import "./index.css";
 import App from "./App.tsx";
 import { ErrorBoundary } from "react-error-boundary";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { BrowserRouter } from "react-router";
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  ScrollRestoration,
+} from "react-router";
+import PokemonDetails from "./pages/pokemon_page.tsx/pokemon_details.tsx";
+import BerryLayout from "./pages/berry_page/berry_layout.tsx";
+import BerryDetails from "./pages/berry_page/berry_details.tsx";
+import ItemDetails from "./pages/item_page/item_details.tsx";
+import ItemLayout from "./pages/item_page/item_layout.tsx";
+import Home from "./pages/homepage/home_page.tsx";
+import PokemonLayout from "./pages/pokemon_page.tsx/pokemon_layout.tsx";
 
 const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <ErrorBoundary
+        fallback={
+          <div className="bg-red-700">React said: Something went wrong</div>
+        }
+      >
+        <App />
+      </ErrorBoundary>
+    ),
+    children: [
+      { index: true, element: <Home /> },
+      { path: "home", element: <Home /> },
+
+      {
+        path: "pokemon",
+        element: <Layout />,
+        children: [
+          { index: true, element: <PokemonLayout /> },
+          { path: ":name", element: <PokemonDetails /> },
+        ],
+      },
+      {
+        path: "berry",
+        element: <Layout />,
+        children: [
+          { index: true, element: <BerryLayout /> },
+          { path: ":name", element: <BerryDetails /> },
+        ],
+      },
+      {
+        path: "item",
+        element: <Layout />,
+        children: [
+          { index: true, element: <ItemLayout /> },
+          { path: ":name", element: <ItemDetails /> },
+        ],
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <ErrorBoundary
-          fallback={
-            <div className="bg-red-700">React said: Something went wrong</div>
-          }
-        >
-          <App />
-          {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-        </ErrorBoundary>
-      </BrowserRouter>
+      <ErrorBoundary
+        fallback={
+          <div className="bg-red-700">React said: Something went wrong</div>
+        }
+      >
+        <RouterProvider router={router} />
+
+        {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+      </ErrorBoundary>
     </QueryClientProvider>
   </StrictMode>,
 );
+
+export default function Layout() {
+  return (
+    <>
+      <Outlet />
+      <ScrollRestoration />
+    </>
+  );
+}
