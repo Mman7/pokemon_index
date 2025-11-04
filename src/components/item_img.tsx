16bd8abda56acf87692497c4aa2094b34d3e057c
EricMan <youzai0427@gmail.com>
2025-11-04 16:50:50 +0800

    adjusted: skeleton size

--- src/components/item_img.tsx
+++ src/components/item_img.tsx
@@ -11,12 +11,14 @@ export default function ItemImg({
     <figure className="flex items-center justify-center rounded-xl bg-gray-200/50 dark:bg-black/15">
       {src ? (
         <img
-          className={`${!imgClassName && "m-8"} w-32 ${imgClassName}`}
+          className={`${!imgClassName && "m-8"} w-32 ${imgClassName} `}
           src={src}
           alt={alt}
         />
       ) : (
-        <div className="h-32 p-5 text-center text-6xl">?</div>
+        <div className="grid h-38 w-38 justify-center justify-items-center p-6 text-center text-6xl">
+          ?
+        </div>
       )}
     </figure>
   );
