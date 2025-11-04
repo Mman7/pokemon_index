b5a5498083d9dfd31943a21c0d910307dae584c1
EricMan <youzai0427@gmail.com>
2025-11-04 16:50:24 +0800

    fixed chart value

--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -40,12 +40,16 @@ export default function Charts({
   };
 
   useEffect(() => {
+    if(chartType == 'bar'){
+      setMaxValue(10);
+      return
+    }
     for (let index = 0; index < stats.length; index++) {
       const element = stats[index];
-      if (element <= 10) setMaxValue(10);
+      if (element > 200) setMaxValue(250);
       if (element > 100) setMaxValue(150);
       if (element > 150) setMaxValue(200);
-      if (element > 200) setMaxValue(250);
+      if (element > 100) setMaxValue(150);
     }
   }, []);
 
