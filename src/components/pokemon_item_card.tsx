4749cac3fbff0ae8b2981c2fad30517edd7b97bb
EricMan <youzai0427@gmail.com>
2025-11-04 16:51:47 +0800

    added inview to reduce render

--- src/components/pokemon_item_card.tsx
+++ src/components/pokemon_item_card.tsx
@@ -4,15 +4,18 @@ import { TypeBadges } from "./type_badges";
 import { getPokemonDetailByName } from "../api/pokemon_api";
 import ItemCardSkeleton from "./item_card_skeleton";
 import ItemImg from "./item_img";
+import { useInView } from "react-intersection-observer";
 
 interface PokemonCardProps {
   name: string;
   pokemonImgClassName: string;
+  itemClassName?: string;
 }
 
 export default function PokemonCard({
   name,
   pokemonImgClassName,
+  itemClassName,
 }: PokemonCardProps) {
   const location = useLocation();
   const {
@@ -26,14 +29,16 @@ export default function PokemonCard({
     refetchOnMount: false,
     refetchOnWindowFocus: false,
   });
+  const { ref, inView } = useInView({
+    triggerOnce: true, // Only trigger once per item
+    rootMargin: "200px", // Render slightly before it enters view
+  });
 
   if (isLoading) return <ItemCardSkeleton />;
-
   if (error && error.message == "Request failed with status code 404")
     return (
       <div className="grid h-3/4 justify-items-center">Pokemon not found</div>
     );
-
   if (error)
     return <div className="grid h-3/4 justify-items-center">error.message</div>;
   return (
@@ -44,33 +49,42 @@ export default function PokemonCard({
         pokemon: baseData,
       }}
     >
-      <div className="card rounded-xl shadow-lg backdrop-brightness-120 duration-1000 hover:cursor-pointer hover:backdrop-brightness-140">
-        <figure className="p-8">
-          <div className="indicator rounded-2xl bg-black/5">
-            <div className="indicator-item indicator-center badge flex gap-5 border-0 bg-transparent">
-              {baseData?.types.map((a) => (
-                <TypeBadges
-                  fontSize="text-base"
-                  key={a.type.name}
-                  type={a.type.name}
-                />
-              ))}
+      <div
+        ref={ref}
+        className={`${itemClassName} card rounded-xl shadow-lg backdrop-brightness-120 transition-transform duration-200 hover:scale-105 hover:cursor-pointer hover:backdrop-brightness-140 active:scale-95`}
+      >
+        {inView && isLoading ? (
+          <ItemCardSkeleton />
+        ) : (
+          <figure className="p-8">
+            <div className={`indicator rounded-2xl bg-black/5`}>
+              <div className="indicator-item indicator-center badge flex gap-5 border-0 bg-transparent">
+                {baseData?.types.map((pokemon) => (
+                  <TypeBadges
+                    fontSize="text-base"
+                    key={pokemon.type.name}
+                    type={pokemon.type.name}
+                  />
+                ))}
+              </div>
+              <span
+                className={`${name.length > 10 && "text-xs"} indicator-item indicator-bottom indicator-center badge bg-gray-500 px-3 py-3.5 text-base font-medium text-white capitalize dark:bg-gray-600`}
+              >
+                {name}
+              </span>
+              <span
+                className={`${name.length > 13 && "hidden"} ${name.length < 13 && "text-xl"} indicator-item indicator-start indicator-bottom badge bg-gray-500 p-1.5 text-xs font-medium text-gray-300 dark:bg-gray-600`}
+              >
+                #{baseData?.id}
+              </span>
+              <ItemImg
+                imgClassName={pokemonImgClassName}
+                src={baseData?.sprites.front_default ?? undefined}
+                alt={baseData?.name}
+              />
             </div>
-            <span
-              className={`${name.length > 10 && "text-xs"} indicator-item indicator-bottom indicator-center badge bg-gray-500 px-3 py-3.5 text-base font-medium text-white capitalize dark:bg-gray-600`}
-            >
-              {name}
-            </span>
-            <span className="indicator-item indicator-start indicator-bottom badge bg-gray-500 p-1.5 text-xs font-medium text-gray-300 dark:bg-gray-600">
-              #{baseData?.id}
-            </span>
-            <ItemImg
-              imgClassName={pokemonImgClassName}
-              src={baseData?.sprites.front_default ?? undefined}
-              alt={baseData?.name}
-            />
-          </div>
-        </figure>
+          </figure>
+        )}
       </div>
     </Link>
   );
