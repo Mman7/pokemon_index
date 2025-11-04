bac49e2bc78f98b5eda016904a9f92c406643fe2
EricMan <youzai0427@gmail.com>
2025-11-04 16:51:33 +0800

    refactor

--- src/components/item_list.tsx
+++ src/components/item_list.tsx
@@ -1,30 +1,19 @@
 import type { NamedAPIResource } from "pokenode-ts";
 import type { ReactElement } from "react";
+import React from "react";
 
-export default function ItemList({
+export default (function ItemList({
   items,
-  isShowing,
-  refs,
   children,
 }: {
   items: NamedAPIResource[];
-  isShowing: boolean;
-  refs: any;
   children: (item: NamedAPIResource) => ReactElement;
 }) {
   return (
     <>
       {items.map((item) => (
-        <div
-          key={item.name}
-          className={isShowing ? "block" : "hidden"}
-          ref={(el) => {
-            refs.current[item.name] = el;
-          }}
-        >
-          {children(item)}
-        </div>
+        <React.Fragment key={item.name}>{children(item)}</React.Fragment>
       ))}
     </>
   );
-}
+});
