25f305f89a6b94de1fda9397e173439802df01e1
Eric Man <youzai0427@gmail.com>
2026-02-04 11:48:45 +0800

    feat: Enhance search functionality and UI improvements

--- src/components/item_card_skeleton.tsx
+++ src/components/item_card_skeleton.tsx
@@ -1,9 +1,8 @@
 export default function ItemCardSkeleton() {
   const defaultStyle = "skeleton ml-auto bg-gray-300 dark:bg-gray-700";
   return (
-    <div className="flex w-52 flex-col gap-4">
-      <div className={`${defaultStyle} h-32 w-full`}></div>
-      <div className={`${defaultStyle} h-8 w-full`}></div>
+    <div className="flex flex-col gap-4">
+      <div className={`${defaultStyle} h-50 w-full`}></div>
       <div className={`${defaultStyle} h-8 w-full`}></div>
     </div>
   );
