5225002e7cb60a61e781bd316717acd4114d53d0
Eric Man <youzai0427@gmail.com>
2026-02-04 14:55:05 +0800

    feat: added animate pulse

--- src/components/item_card_skeleton.tsx
+++ src/components/item_card_skeleton.tsx
@@ -1,7 +1,7 @@
 export default function ItemCardSkeleton() {
   const defaultStyle = "skeleton ml-auto bg-gray-300 dark:bg-gray-700";
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex animate-pulse flex-col gap-4">
       <div className={`${defaultStyle} h-50 w-full`}></div>
       <div className={`${defaultStyle} h-8 w-full`}></div>
     </div>
